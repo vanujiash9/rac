a84aa8daa579de57fea46d0bceea23ab941dad8e
unknown <thanh.van19062004@gmail.com>
2024-06-14 18:56:49 +0700

    first commit

--- components/product.jsx
+++ components/product.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function Product({ item }) {
   return (
     <div className="flex flex-col items-center m-6">
-      <div className="flex flex-col justify-center items-center border rounded-lg p-4">
-        <img className="h-56" src={item.imgURL} alt={item.name} />
-        <h2 className="font-black text-lg">{item.name}</h2>
-        <h3 className="text-sm">
-          Giá: <strong>{item.price} VNĐ</strong>
+      <div className="flex flex-col justify-center items-center border rounded-lg p-4 shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out transform hover:scale-105">
+        <img className="h-56 w-56 object-cover rounded-md mb-4" src={item.imgURL} alt={item.name} />
+        <h2 className="font-bold text-lg text-gray-800">{item.name}</h2>
+        <h3 className="text-sm text-gray-600 mt-2">
+          Giá: <strong className="text-red-500">{item.price} VNĐ</strong>
         </h3>
       </div>
     </div>
